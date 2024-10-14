2b3a6578d6e20386492a30ea76eafb5e2713f166
Andi Fajar <andi.fajar@traveloka.com>
2024-10-14 14:00:31 +0700

    Better regula docs ux

--- src/regula/RegulaDocsReader.jsx
+++ src/regula/RegulaDocsReader.jsx
@@ -1,5 +1,5 @@
-import { useState, useRef, useEffect } from 'react';
-import { Segment, Container, Header, Button, Grid, GridRow, GridColumn, Modal, Icon, ModalContent, ModalActions } from 'semantic-ui-react';
+import { useState, useRef, useEffect, useCallback } from 'react';
+import { Segment, Header, Button, Grid, GridRow, GridColumn, Modal, Icon, ModalContent, ModalActions } from 'semantic-ui-react';
 import { isEmpty } from 'lodash';
 import { useNavigate } from 'react-router-dom';
 import ResultTabs from './result/ResultTab';
@@ -25,6 +25,7 @@ const RegulaDocsReader = () => {
     const api = new DocumentReaderApi(configuration);
     const [readerResult, setReaderResult] = useState({});
     const [isLoading, setLoading] = useState(false);
+    const [documentCheckStarted, setDocumentCheckStarted] = useState(false)
 
     const [checkRequest, setCheckRequest] = useState({});
     const [checkResponse, setCheckResponse] = useState({});
@@ -32,8 +33,7 @@ const RegulaDocsReader = () => {
 
     const containerStyle = {
         justifyContent: 'center',
-        alignItems: 'center',
-        maxWidth: '1024px'
+        alignItems: 'center'
       };
 
     const showResult = async () => {
@@ -60,31 +60,36 @@ const RegulaDocsReader = () => {
         setCheckRequest(processParam);
     }
 
+    const regulaListener = useCallback(async (data) => {
+        if (data.detail.action === EventActions.PROCESS_FINISHED) {
+            const status = data.detail.data?.status;
+            const isFinishStatus = status === 1;
+
+            if (!isFinishStatus || !data.detail.data?.response) return;
+            // console.log(data.detail.data.response);
+            // TODO: try end to end server validation
+            //window.RegulaDocumentSDK.finalizePackage().then((item) => { console.log(item) });
+            // requestOcr(data.detail.data.response);
+            setReaderResult(data.detail.data.response)
+            await new Promise(r => {
+                setTimeout(r, 700);
+                setDocumentCheckStarted(false);
+            });
+        }
+
+        if (data.detail?.action === EventActions.CLOSE) {
+            navigate("/");
+        }
+    }, []);
+
     useEffect(() => {
-        const containerCurrent = containerRef.current;
-        const listener = (data) => {
-            if (data.detail.action === EventActions.PROCESS_FINISHED) {
-                const status = data.detail.data?.status;
-                const isFinishStatus = status === 1;
-
-                if (!isFinishStatus || !data.detail.data?.response) return;
-                // console.log(data.detail.data.response);
-                // TODO: try end to end server validation
-                //window.RegulaDocumentSDK.finalizePackage().then((item) => { console.log(item) });
-                // requestOcr(data.detail.data.response);
-                setReaderResult(data.detail.data.response)
-            }
-
-            if (data.detail?.action === EventActions.CLOSE) {
-                navigate("/");
-            }
-        };
-
+        console.log("INIT FIRST TIME")
         void defineComponents().then(() => window.RegulaDocumentSDK.initialize({ license: 'AAEAAKuvmF430Aay0Ef9dG0Wmq73sE5BjW9l+N1vXOPPI5ZoQnFPMGE8Co3KojvtQib0M0PR5O+6l43x1wTBDifFY3yIKYUjRoomOdz4V2t4SEVehsia1xW0u8ZKc0N9IpSxPrREjCETQT9wnLrkZHwyZAIfLNRF+H+xUI0tkMImXwa6pvM/lzcvh+cfwGU/Tmg5cDakkqnHALlxYg1TfsmQkTOAZPI9hl7tEbxla2W8FucmwXEhKaPLRvcKrI/5Em8ZK+0iZ+bu1RRqAkUQfLhLym0si9HaLGRNAGsR6WD7ZfJkf5kL6polf/JaLaWESRY4QxvhHQUzHgq/+t5q9SDVuUnkAAAAAAAAEPJ86bDHkhTQkmx6xYF73dn+tfyT1maGHGl0dDMuBTI1ovI0dXCqoPAhR6BduAF7Ka2fM3YeqR2rE/8W2H5olwQ58t0ico9bYB6Td0oo+IDiG+l/Alacb1ppP4CbaQB1DeZAOphdTro63PjwdGYWXFEelAtZScNd+IOovGipufG36X+wIvnpx4MTvoBQySsmvU8kMHFeB+VP2a/1uKLVAYr5XC9OTTH2PZ4mevXBMr0kWoYtHae9tQOdchNuTEG5SmLQt2vNcCOt7uGzheIA6qHdGMCoT+pstf4FZGzfobMr' }));
 
+        const containerCurrent = containerRef.current;
         if (!containerCurrent) return;
 
-        containerCurrent.addEventListener('document-reader', listener);
+        containerCurrent.addEventListener('document-reader', regulaListener);
 
         const elementRefCurrent = elementRef.current;
         
@@ -103,90 +108,133 @@ const RegulaDocsReader = () => {
         console.log(elementRefCurrent.settings);
         console.log(elementRefCurrent);
         return () => {
-            containerCurrent.removeEventListener('document-reader', listener);
+            window.RegulaDocumentSDK.shutdown();
+            containerCurrent.removeEventListener('document-reader', regulaListener);
         }
-    }, []);
+    }, [regulaListener]);
+
+    useEffect(() => {
+        const elementRefCurrent = elementRef.current;
+        
+        if (!elementRefCurrent) return;
+        
+        elementRefCurrent.settings = {
+            serviceUrl: 'https://mfcrgla.mfc.staging-traveloka.com',
+            regulaLogo: false,
+            internalScenario: InternalScenarios.Locate,
+            captureButton: true,
+            changeCameraButton: true,
+            closeButton: false,
+            captureMode: 'auto',
+            cameraMode: 'environment'
+        };
+        console.log(elementRefCurrent.settings);
+        console.log(elementRefCurrent);
+    }, [documentCheckStarted])
 
     return (
         <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-        <Segment inverted color="blue" style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          right: 0,
-          height: '60px',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          margin: 0,
-          borderRadius: 0
-        }} textAlign="center" padded="very">
-          <Header as="h1" content="Regula Doc" />
-        </Segment>
-        <Grid divided='vertically' style={{
-                        position: 'absolute',
-                        top: '100px',
-                        bottom: '100px',
-                        left: 0,
-                        right: 0,
-                        display: 'flex',
-                        alignItems: 'center',
-                        justifyContent: 'center'
-                        }}>
-            <GridRow columns={1} centered>
-                <GridColumn textAlign='center' centered>
-                    <div ref={containerRef} style={containerStyle}>
+            <div ref={containerRef} style={containerStyle}>
+            {
+                documentCheckStarted ? (
                         <document-reader ref={elementRef}></document-reader>
-                    </div>
-                </GridColumn>
-            </GridRow>
-            <GridRow columns={2}>
-                <GridColumn textAlign='center'>
-                    <Button onClick={() => navigate("/")}>Back</Button>
-                </GridColumn>
-                <GridColumn textAlign='center'>
-                    <Button loading={isLoading} disabled={isEmpty(readerResult) || isLoading} onClick={() => showResult()}>Show result</Button>
-                </GridColumn>
-            </GridRow>
-        </Grid>
-  
+                ) : (
+        <>
+            <Segment inverted color="blue" style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            height: '60px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            margin: 0,
+            borderRadius: 0
+            }} textAlign="center" padded="very">
+            <Header as="h1" content="Regula Doc" />
+            </Segment>
+            <Grid divided='vertically' style={{
+                            position: 'absolute',
+                            top: '100px',
+                            bottom: '100px',
+                            left: 0,
+                            right: 0,
+                            display: 'flex',
+                            alignItems: 'center',
+                            justifyContent: 'center'
+                            }}>
+                {
+                    isEmpty(readerResult) ? (
+                        <GridRow columns={1} centered>
+                            <GridColumn textAlign='center' centered>
+                                <Header as="h4">Please prepare your ID Card</Header>
+                            </GridColumn>
+                            <GridColumn textAlign='center' centered>
+                                <Button onClick={() => setDocumentCheckStarted(true)}>Click when ready</Button>
+                            </GridColumn>
+                        </GridRow>
+                    ) : (
+                        <GridRow columns={1} centered>
+                            <GridColumn textAlign='center' centered>
+                                <Header as="h3">ID Card Check Completed!</Header>
+                            </GridColumn>
+                        </GridRow>
+                    )
+                }                
+                
+                <GridRow columns={2}>
+                    <GridColumn textAlign='center'>
+                        <Button onClick={() => navigate("/")}>Back</Button>
+                    </GridColumn>
+                    <GridColumn textAlign='center'>
+                        <Button loading={isLoading} disabled={isEmpty(readerResult) || isLoading} onClick={() => showResult()}>Show result</Button>
+                    </GridColumn>
+                </GridRow>
+            </Grid>
     
-        <Modal
-            closeIcon
-            onClose={() => setIsResultOpen(false)}
-            onOpen={() => setIsResultOpen(true)}
-            open={isResultOpen}
-            size='small'
-        >
-            <Header icon='archive' content='Result' />
-            <ModalContent>
-                <ResultTabs response={checkResponse} request={checkRequest} language="en" ></ResultTabs>
-            </ModalContent>
-            <ModalActions>
-            <Button basic onClick={() => navigator.clipboard.writeText(JSON.stringify(checkResponse))}>
-                <Icon name='copy' /> Copy Raw Response
-            </Button>
-            <Button basic color='red' onClick={() => setIsResultOpen(false)}>
-                <Icon name='remove' /> Close
-            </Button>
-            </ModalActions>
-        </Modal>
+        
+            <Modal
+                closeIcon
+                onClose={() => setIsResultOpen(false)}
+                onOpen={() => setIsResultOpen(true)}
+                open={isResultOpen}
+                size='small'
+            >
+                <Header icon='archive' content='Result' />
+                <ModalContent>
+                    <ResultTabs response={checkResponse} request={checkRequest} language="en" ></ResultTabs>
+                </ModalContent>
+                <ModalActions>
+                <Button basic onClick={() => navigator.clipboard.writeText(JSON.stringify(checkResponse))}>
+                    <Icon name='copy' /> Copy Raw Response
+                </Button>
+                <Button basic color='red' onClick={() => setIsResultOpen(false)}>
+                    <Icon name='remove' /> Close
+                </Button>
+                </ModalActions>
+            </Modal>
+        
     
-  
-        <Segment style={{
-          position: 'absolute',
-          bottom: 0,
-          left: 0,
-          right: 0,
-          height: '60px',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          margin: 0,
-          borderRadius: 0
-        }} inverted textAlign="center" padded="very">
-          <p>&copy; 2024 CSF</p>
-        </Segment>
+            <Segment style={{
+            position: 'absolute',
+            bottom: 0,
+            left: 0,
+            right: 0,
+            height: '60px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            margin: 0,
+            borderRadius: 0
+            }} inverted textAlign="center" padded="very">
+            <p>&copy; 2024 CSF</p>
+            </Segment>
+        </>  
+                )
+            }
+        </div>
+          
       </div>
     )
 }
