c1af96a99bc461435f33022176c9d9ab4da14b4a
Andi Fajar <andi.fajar@traveloka.com>
2024-10-30 10:47:05 +0700

    fix

--- src/aai/AaiDocs.jsx
+++ src/aai/AaiDocs.jsx
@@ -146,13 +146,13 @@ const AaiDocs = () => {
                     <Header as="h4">ID Forgery Check</Header>
                 </Grid.Column>
                 <Grid.Column>
-                    <Icon name={get(idForgeryResponse, 'data.result') === 'fail' ? 'dont' : 'check circle'} size='large' color={get(idForgeryResponse, 'data.result') === 'fail' ? 'red' : 'green'}>{get(idForgeryResponse, 'data.result').toUpperCase()}</Icon>
+                    <Icon name={get(idForgeryResponse, 'data.result') === 'pass' ? 'check circle' : 'dont'} size='large' color={get(idForgeryResponse, 'data.result') === 'pass' ? 'green' : 'red'}>{get(idForgeryResponse, 'data.result', '-').toUpperCase()}</Icon>
                 </Grid.Column>
                 <GridColumn>
-                    <InfoField label="ID Forgery Check Result" value={get(idForgeryResponse, 'data.result').toUpperCase()} />
+                    <InfoField label="ID Forgery Check Result" value={get(idForgeryResponse, 'data.result', '-').toUpperCase()} />
                 </GridColumn>
                 <GridColumn>
-                    <InfoField label="Reason" value={get(idForgeryResponse, 'data.detail')} />
+                    <InfoField label="Reason" value={get(idForgeryResponse, 'data.detail', '-')} />
                 </GridColumn>
               </GridRow>
             }
